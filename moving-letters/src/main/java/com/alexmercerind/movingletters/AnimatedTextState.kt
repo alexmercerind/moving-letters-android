fdb955cb3d032ed4d5fe3c853553d4000df94af7
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-12-11 17:58:17 +0530

    refactor: AnimatedTextState API

--- moving-letters/src/main/java/com/alexmercerind/movingletters/AnimatedTextState.kt
+++ moving-letters/src/main/java/com/alexmercerind/movingletters/AnimatedTextState.kt
@@ -13,7 +13,14 @@ import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlin.time.Duration
 
@@ -21,12 +28,37 @@ import kotlin.time.Duration
  * A state object that can be hoisted to control and observe the animated text state.
  * In most cases, this state will be created via [rememberAnimatedTextState].
  */
+@OptIn(DelicateCoroutinesApi::class)
 class AnimatedTextState() {
+    /** Whether animation is currently paused. */
+    val paused: StateFlow<Boolean> get() = _paused
+
+    /** Whether animation is currently stopped. */
+    val stopped: StateFlow<Boolean> get() = _stopped
+
+    /** Whether animation is currently playing (not paused). */
+    val playing get() = paused.map { !it }
+
+    /** Whether animation is currently ongoing (not stopped). */
+    val ongoing get() = stopped.map { !it }
+
     /** Whether this instance is attached to an animated text. */
     internal var attached = false
 
     /** Whether initial text layout has been done. */
     internal val layout = CompletableDeferred<Any>()
+
+    /** Current visible character index. */
+    internal var current by mutableStateOf(-1)
+
+    /** Current animation coroutine job. */
+    private var job: Job? = null
+
+    /** Current animation coroutine jobs. */
+    private var jobs: MutableSet<Job> = mutableSetOf()
+
+    private var _paused = MutableStateFlow(true)
+    private var _stopped = MutableStateFlow(true)
 
     private var _visibility: List<MutableState<Boolean>>? = null
     private var _transformOrigin: MutableList<TransformOrigin>? = null
@@ -67,29 +99,55 @@ class AnimatedTextState() {
             _intermediateDuration = value
         }
 
-    internal var current by mutableStateOf(-1)
+    fun start() {
+        stop()
+        resume()
+        _stopped.update { false }
+    }
 
-    suspend fun reset() {
-        layout.await()
+    fun stop() {
+        pause()
         current = -1
         for (i in visibility.indices) {
             visibility[i].value = false
         }
+        _stopped.update { true }
     }
 
-    @OptIn(DelicateCoroutinesApi::class)
-    suspend fun start() {
-        layout.await()
-        reset()
-        for (i in visibility.indices) {
-            delay(intermediateDuration)
-            visibility[i].value = true
-            GlobalScope.launch(Dispatchers.IO) {
-                delay(animationDuration)
-                current = i
-                delay(100)
-                visibility[i].value = false
+    fun resume() {
+        if (paused.value) {
+            job = GlobalScope.launch(Dispatchers.IO) {
+                layout.await()
+                for (i in current.coerceIn(0, 1 shl 16) until visibility.size) {
+                    if (coroutineContext.isActive) {
+                        delay(intermediateDuration)
+                        visibility[i].value = true
+                        jobs.add(
+                            async {
+                                delay(animationDuration)
+                                current = i
+                                delay(100)
+                                visibility[i].value = false
+
+                                if (current == visibility.size - 1) {
+                                    _stopped.update { true }
+                                }
+                            }
+                        )
+                    }
+                }
             }
+            _paused.update { false }
+        }
+    }
+
+    fun pause() {
+        if (!paused.value) {
+            job?.cancel()
+            job = null
+            jobs.forEach { it.cancel() }
+            jobs.clear()
+            _paused.update { true }
         }
     }
 }
